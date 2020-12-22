b6549275f1c1662b6f45e8dc3607501fce8f21bb
James <jameslayson25@gmail.com>
2020-12-22 19:54:48 +0800

    first commit

---
+++ Client/Client/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket master = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint ipEnd = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8888);
+            master.Connect(ipEnd);
+
+            string sendData = "";
+
+            do
+            {
+                Console.Write("Send Message: ");
+                sendData = Console.ReadLine();
+                master.Send(System.Text.Encoding.UTF8.GetBytes(sendData));
+            } while (sendData.Length > 0);
+
+            master.Close();
+        }
+    }
+}
---
+++ Server/Server/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket listenerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint ipEnd = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8888);
+            listenerSocket.Bind(ipEnd);
+            listenerSocket.Listen(0);
+            Socket clientSocket = listenerSocket.Accept();
+            byte[] Buffer = new byte[clientSocket.SendBufferSize];
+
+            int readByte;
+            do
+            {
+                readByte = clientSocket.Receive(Buffer);
+                byte[] rData = new byte[readByte];
+                Array.Copy(Buffer, rData, readByte);
+                Console.WriteLine("New Message: " + System.Text.Encoding.UTF8.GetString(rData));
+            } while (readByte > 0);
+
+            Console.WriteLine("Client Disconected");
+            Console.ReadKey();
+        }
+    }
+}
